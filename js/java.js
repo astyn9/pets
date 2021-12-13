51f069a3552ecf1a4dcffc9a01b14773aa9c028c
astyn9 <gabriel.bernardino@urca.br>
2021-12-13 19:51:46 -0300

    confirm

---
+++ js/java.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#submit").on("click", function(){
+        $("#dialog").dialog();
+    });
+});
+
+$(document).ready(function(){
+    $("#submit2").on("click", function(){
+        $("#dialog").dialog();
+    });
+});
